2b3ae36858371a9ad07461fcaf6e4438b006738a
AngelJMavare <angeljmavare@gmail.com>
2025-01-19 18:04:16 -0400

    FEAT: add hack_8

--- hack_8/hack_8.js
+++ hack_8/hack_8.js
@@ -9,8 +9,15 @@
  * output => ["b@r","b@z","QUX"]   
  */
 let arr = ["foo","bar","baz","qux","echo"];
+arr.shift();
+arr.pop();
+arr= arr.map((value) => value.replace("a","@"));
+let tam = arr.length;
+arr[tam-1] = arr[tam-1].toUpperCase();
+
 let result = [];
 
+result = arr.map((value) => value);
 
 //export result
 module.exports = result;
