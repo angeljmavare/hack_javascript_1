7c791458648c7dfae117f3bdb38d2ff85c8bc5df
AngelJMavare <angeljmavare@gmail.com>
2025-01-19 18:07:14 -0400

    FEAT: add hack_9

--- hack_9/hack_9.js
+++ hack_9/hack_9.js
@@ -8,7 +8,13 @@
  */
 let arr = ["foo","bar","baz","qux","echo"];
 let result = 0;
+let tam = arr.length;
 
+for(let i = 0; i < tam; i++){
+    if (arr[i].startsWith("b")) {
+        result++;
+    }
+}
 
 //export result
 module.exports = result;
