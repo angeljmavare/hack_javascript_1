c1c2ccd75563f8b1e29cb2d109a1511f4049342d
AngelJMavare <angeljmavare@gmail.com>
2025-01-19 17:09:55 -0400

    FEAT: add hack_5

--- hack_5/hack_5_map.js
+++ hack_5/hack_5_map.js
@@ -5,8 +5,12 @@
  * 
  * output => [7,5,3,1]
  */
-let result = [];
 
+let number = [7,6,5,4,3,2,1];
+let newArray = number.filter((value) => value % 2 ==! 0);
+
+let result = [];
+result = newArray.map((value) => value);
 
 //export result
 module.exports = result;
--- hack_5/hack_5_while.js
+++ hack_5/hack_5_while.js
@@ -5,8 +5,14 @@
  * 
  * output => [7,5,3,1]
  */
-let result = [];
 
+let result = [];
+let number= 7;
+
+while(number > 0){
+    result.push(number);
+    number-=2;
+}
 
 //export result
 module.exports = result;
