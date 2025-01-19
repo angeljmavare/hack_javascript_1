22aa224471c5bb59140a24f2bca67e12d34e3c6e
AngelJMavare <angeljmavare@gmail.com>
2025-01-19 18:43:02 -0400

    FEAT: add hack_11

--- hack_11/hack_11.js
+++ hack_11/hack_11.js
@@ -16,9 +16,25 @@
  * output => ["h@ck","one",2,"three",4,"five","h@ck","f00","Bar","b@z","quX","3ch0","h@ck"]   
  */
 let numberArray = [1,2,3,4,5];
+let letterArray = ["one","two","three","four","five"];
 let stringArray = ["foo","bar","baz","qux","echo"]
+
+stringArray = stringArray.map(value => value .replace("foo", "f00") .replace("bar", "Bar") .replace("baz", "b@z") .replace("qux", "quX") .replace("echo", "3ch0"));
+
 let result = [];
 
+for(let i = 0; i < (numberArray.length); i++){
+    if (i % 2 ==! 0){
+        result.push(numberArray[i]);
+    }
+    else { 
+        result.push(letterArray[i]);
+    }	
+}
+result.unshift("h@ck");
+result.push("h@ck");
+result = result.concat(stringArray);
+result.push("h@ck");
 
 //export result
 module.exports = result;
