c7b449a34e67e9c52afb925a43d83e23036fa943
AngelJMavare <angeljmavare@gmail.com>
2025-01-19 17:32:33 -0400

    FEAT: hack_7

--- hack_7/hack_7.js
+++ hack_7/hack_7.js
@@ -7,8 +7,14 @@
  * output => ["Foo","Bar","Baz","Qux","Echo"]   
  */
 let arr = ["foo","bar","baz","qux","echo"];
+let newArray = [];
 let result = [];
 
+function mayusPrimeraLetra(palabra) {
+    return palabra.charAt(0).toUpperCase()+palabra.slice(1); 
+}
+
+result = arr.map((value) => mayusPrimeraLetra(value));
 
 //export result
 module.exports = result;
